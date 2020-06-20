045122b3fb3b8976eb9893aa66d2c2391070105f
Xuezheng Wang <xzzwang@icloud.com>
2020-06-20 15:17:56 -0700

    Initial Commit

---
+++ MetalRaytracer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MetalRaytracer
+//
+//  Created by Xuezheng Wang on 6/20/20.
+//  Copyright © 2020 Xuezheng Wang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
